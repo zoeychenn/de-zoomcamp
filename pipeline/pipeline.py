28e5bd54ab356a0db75653791a2cb04b2c38f05c
Zoey  Chen <40010142+zoeychenn@users.noreply.github.com>
2026-01-14 03:33:31 +0000

    de pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,12 @@
+import sys
+import pandas as pd
+
+print("arguments", sys.argv)
+
+month = int(sys.argv[1])
+
+df=pd.DataFrame({"day":[1,2],"num_passengers":[3,4]})
+df["month"]=month
+print(df.head())
+df.to_parquet(f"output_{month}.parquet")
+print(f"Running pipeline for day {month}")
