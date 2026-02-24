3a7a1d66f468c9ea522772d1c952384b5dfdfdf6
Zoey  Chen <40010142+zoeychenn@users.noreply.github.com>
2026-02-23 21:42:35 -0800

    Update stg_fhv_tripdata.sql

--- dbt/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
+++ dbt/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
@@ -5,7 +5,7 @@ with source as (
 renamed as (
     select
         -- identifiers
-        cast(dispatching_base_num as string) as dispatching_base_num,
+        cast(dispatching_base_num as varchar) as dispatching_base_num,
         cast(PUlocationID as integer) as pickup_location_id,
         cast(DOlocationID as integer) as dropoff_location_id,
 
@@ -15,15 +15,9 @@ renamed as (
 
         -- trip info
         cast(SR_Flag as string) as store_and_fwd_flag,
-        cast(Affiliated_base_number as string) as affiliated_base_number
+        cast(Affiliated_base_number as varchar) as affiliated_base_number
     from source
-    -- Filter out records with null vendor_id (data quality requirement)
     where dispatching_base_num is not null
 )
 
 select * from renamed
-
--- Sample records for dev environment using deterministic date filter
-{% if target.name == 'dev' %}
-where pickup_datetime >= '2019-01-01' and pickup_datetime < '2019-02-01'
-{% endif %}
